64d183b18f6202378153c28ac1b02b4fd3458af8
Daniela77 <danielacolamai@gmail.com>
2015-11-17 20:21:24 -0300

    pass y usuarios model

---
+++ model/usuarios_model.php
@@ -0,0 +1,13 @@
+<?php
+include_once "model/model.php";
+
+class UsuariosModel extends Model {
+
+  function getUsuario($email){
+    $consulta = $this->db->prepare("SELECT * FROM usuario WHERE email = ?");
+    $consulta->execute(array($email));
+    return $consulta->fetch();
+  }
+
+}
+?>
